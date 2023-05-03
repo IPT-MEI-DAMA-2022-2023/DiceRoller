c5e6d0358129b91a2360fe64dd41d6091568274b
jcnpereira <casimiro@ipt.pt>
2023-05-03 21:31:48 +0100

    add a textview to show the dice values

--- app/src/main/java/pt/ipt/dama/diceroller/MainActivity.kt
+++ app/src/main/java/pt/ipt/dama/diceroller/MainActivity.kt
@@ -2,10 +2,34 @@ package pt.ipt.dama.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // find the button
+        val auxRollTheDiceButton: Button = findViewById(R.id.rollTheDiceButton)
+        // <=> val auxRollTheDiceButton = findViewById<Button>(R.id.rollTheDiceButton)
+        auxRollTheDiceButton.setOnClickListener {
+            rollTheDice()
+        }
+    }
+
+    /**
+     * 'roll' the dice
+     *  ie, add a random value to the textview
+     */
+    private fun rollTheDice() {
+        // get the random value
+        val randomValue = Random().nextInt(6) + 1 // it will get values from 1 to 6
+        // assign the random value to the TextView
+        val auxDiceNumberTextView: TextView = findViewById(R.id.diceNumberTextView)
+        auxDiceNumberTextView.text = randomValue.toString()
+
+
     }
 }
